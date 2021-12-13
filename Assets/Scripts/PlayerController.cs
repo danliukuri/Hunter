b6dd4e2273ed52a356b42a2dc3763e9ba3592f43
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2021-12-13 23:54:57 +0200

    Create a player Add the ability for the player to move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Methods
+    [SerializeField] float movementSpeed;
+    [SerializeField] float rotationalSpeed;
+    #endregion
+
+    #region
+    void Update()
+    {
+        Move(Input.GetAxis("Vertical"));
+        Rotate(Input.GetAxis("Horizontal"));
+    }
+
+    void Move(float verticalAxisvalue)
+    {
+        if (verticalAxisvalue != 0)
+        {
+            transform.Translate(0f, verticalAxisvalue * movementSpeed * Time.deltaTime, 0f);
+        }
+    }
+    void Rotate(float horizontalAxisvalue)
+    { 
+        if (horizontalAxisvalue != 0)
+        {
+            transform.Rotate(0f, 0f, -horizontalAxisvalue * rotationalSpeed * Mathf.Rad2Deg * Time.deltaTime);
+        }
+    }
+    #endregion
+}
