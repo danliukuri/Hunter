e598761b9717ace7ce67f9d491e817ae1a520db6
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2021-12-25 21:29:16 +0200

    Add the ability for the player to shoot Limited the number of bullets for the player Return bullets to the pool at a certain distance from the player

---
+++ Assets/Scripts/BulletDestroyer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class BulletDestroyer : MonoBehaviour
+{
+    #region Methods
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/GunShootingController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class GunShootingController : MonoBehaviour
+{
+    #region Fields
+    [SerializeField] ObjectsPool bulletsPool;
+    [SerializeField] int numberOfBullets;
+    #endregion
+
+    #region Methods
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && numberOfBullets > 0)
+        {
+            numberOfBullets--;
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        GameObject gameObject = bulletsPool.GetFreeObject();
+
+        Transform gameObjectTransform = gameObject.transform;
+        gameObjectTransform.SetParent(transform.parent);
+        gameObjectTransform.rotation = transform.rotation;
+        gameObjectTransform.localPosition = new Vector3(
+            transform.localPosition.x,
+            transform.localPosition.y + transform.localScale.y / 2f + gameObjectTransform.localScale.y / 2f,
+            transform.localPosition.z);
+        gameObjectTransform.SetParent(bulletsPool.ObjectsParent);
+
+        gameObject.SetActive(true);
+    }
+    #endregion
+}
