9dd79ef31fbe540cb66ff0e33c3ad39709e908d8
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-01-24 00:36:23 +0200

    Create IMovementInputService.cs

---
+++ Assets/Scripts/Infrastructure/InputServices/IMovementInputService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Infrastructure.InputServices
+{
+    public interface IMovementInputService
+    {
+        #region Events
+        public event Action<float> HorizontalAxisValueChanging;
+        public event Action<float> VerticalAxisValueChanging;
+        #endregion
+    }
+}
