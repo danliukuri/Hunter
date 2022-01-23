7bad822681a220a55473f6d510b4c98585753e8c
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-01-24 00:40:30 +0200

    Create StandaloneMovementInput.cs

---
+++ Assets/Scripts/Infrastructure/InputServices/StandaloneMovementInput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace Infrastructure.InputServices
+{
+    public class StandaloneMovementInput : MonoBehaviour, IMovementInputService
+    {
+        #region Fields
+        const string horizontalAxisName = "Horizontal";
+        const string verticalAxisName = "Vertical";
+        const int defaultAxisValue = 0;
+
+        public event Action<float> HorizontalAxisValueChanging;
+        public event Action<float> VerticalAxisValueChanging;
+        #endregion
+
+        #region Methods
+        void Update()
+        {
+            TrackHorizontalAxisValueChange();
+            TrackVerticalAxisValueChange();
+        }
+
+        void TrackHorizontalAxisValueChange()
+        {
+            float horizontalAxisValue = Input.GetAxis(horizontalAxisName);
+            if (horizontalAxisValue != defaultAxisValue)
+                HorizontalAxisValueChanging?.Invoke(horizontalAxisValue);
+        }
+        void TrackVerticalAxisValueChange()
+        {
+            float verticalAxisValue = Input.GetAxis(verticalAxisName);
+            if (verticalAxisValue != defaultAxisValue)
+                VerticalAxisValueChanging?.Invoke(verticalAxisValue);
+        }
+        #endregion
+    }
+}
