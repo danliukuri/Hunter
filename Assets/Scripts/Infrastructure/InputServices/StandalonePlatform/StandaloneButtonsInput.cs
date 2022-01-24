d7c5c44678f8b7ded332a0d32a56e4d8eeb60b89
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-01-24 15:59:22 +0200

    Create StandaloneButtonsInput.cs

---
+++ Assets/Scripts/Infrastructure/InputServices/StandalonePlatform/StandaloneButtonsInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace Infrastructure.InputServices.StandalonePlatform
+{
+    class StandaloneButtonsInput : MonoBehaviour, IFireButtonInputService
+    {
+        #region FireButtonServiceFields
+        const string fireButtonName = "Fire1";
+
+        public event Action FireButtonReleased;
+        public event Action FireButtonPressed;
+        #endregion
+
+        #region CommonMethods
+        void Update()
+        {
+            TrackFireButtonRelease();
+            TrackFireButtonPress();
+        }
+        #endregion
+
+        #region FireButtonServiceMethods
+        void TrackFireButtonRelease()
+        {
+            if (Input.GetButtonUp(fireButtonName))
+                FireButtonReleased?.Invoke();
+        }
+        void TrackFireButtonPress()
+        {
+            if (Input.GetButtonDown(fireButtonName))
+                FireButtonPressed?.Invoke();
+        }
+        #endregion
+    }
+}
