9ecf67bb6539fdac2ce1fedc0f81e256e4391a62
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-01-24 15:52:01 +0200

    Create IFireButtonInputService.cs

---
+++ Assets/Scripts/Infrastructure/InputServices/IFireButtonInputService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Infrastructure.InputServices
+{
+    public interface IFireButtonInputService
+    {
+        #region Events
+        event Action FireButtonReleased;
+        event Action FireButtonPressed;
+        #endregion
+    }
+}
