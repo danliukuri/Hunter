405c41d5da74eff0178542778cb82502e0032635
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-01-31 15:36:41 +0200

    Create ObjectsInstaller.cs and invoke it on bootstrap

---
+++ Assets/Scripts/Infrastructure/ObjectsInstaller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Infrastructure
+{
+    public class ObjectsInstaller : MonoInstaller
+    {
+        #region Fields
+        [SerializeField] GameObject bullets;
+        #endregion
+
+        #region Methods
+        public override void InstallBindings()
+        {
+            GameObject bullets = InstantiateBullets();
+            BulletFactory bulletFactory = bullets.GetComponent<BulletFactory>();
+            BindBulletFactory(bulletFactory);
+        }
+        GameObject InstantiateBullets()
+        {
+            return Container.InstantiatePrefab(bullets);
+        }
+        void BindBulletFactory(BulletFactory bulletFactory)
+        {
+            Container
+                .Bind<BulletFactory>()
+                .FromInstance(bulletFactory)
+                .AsSingle();
+        }
+        #endregion
+    }
+}
